e3d6ddfb212d2dac6106335b24f41966bdd9c56b
Pwcong <pwcong@foxmail.com>
2016-09-23 12:48:38 +0800

    Initial commit

---
+++ app/src/test/java/me/pwcong/radiobuttonviewdemo/ExampleUnitTest.java
@@ -0,0 +1,27 @@
+package me.pwcong.radiobuttonviewdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testFloat(){
+
+        float a=2.3f;
+        int b=(int)a;
+        System.out.print(b);
+
+
+    }
+}
